417923172c691afd0299883fd21dbbce7b8752c6
Leroy Truong <leroytruong@cupofcode.io>
2025-07-07 08:22:02 +0200

    405 fix: resolve irrelevant `GezagNietTeBepalen` and `TijdelijkGeenGezag` being present in a v1 response

--- src/main/java/nl/rijksoverheid/mev/web/api/GezagsmoduleInterfaceApiDelegateService.java
+++ src/main/java/nl/rijksoverheid/mev/web/api/GezagsmoduleInterfaceApiDelegateService.java
@@ -2,9 +2,7 @@ package nl.rijksoverheid.mev.web.api;
 
 import nl.rijksoverheid.mev.exception.GezagException;
 import nl.rijksoverheid.mev.gmapi.BevoegdheidTotGezagService;
-import nl.rijksoverheid.mev.web.api.v1.GezagRequest;
-import nl.rijksoverheid.mev.web.api.v1.GezagResponse;
-import nl.rijksoverheid.mev.web.api.v1.GezagsmoduleInterfaceApiDelegate;
+import nl.rijksoverheid.mev.web.api.v1.*;
 import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -47,11 +45,39 @@ public class GezagsmoduleInterfaceApiDelegateService implements GezagsmoduleInte
         var personenV2 = bevoegdheidTotGezagService.bepaalBevoegdheidTotGezag(gezagRequestV2);
         var personenV1 = personenV2.stream()
             .map(BackwardsCompatibility::downgrade)
+            .map(this::excludeIndirectGezagsrelaties)
             .toList();
 
         var gezagResponse = new GezagResponse();
         gezagResponse.setPersonen(personenV1);
         return ResponseEntity.ok(gezagResponse);
+    }
+
+    /**
+     * Exclude irrelevant <i>gezagsrelaties</i>.
+     * <p>
+     * <i>Gezagsrelaties</i> are irrelevant when <code>minderjarige.burgerservicenummer</code> is not equal to
+     * <code>persoon.burgservicenummer</code> and its type is the following:
+     * <ul>
+     *   <li>GezagNietTeBepalen</li>
+     *   <li>TijdelijkGeenGezag</li>
+     * </ul>
+     *
+     * @param persoon <i>persoon</i> whose gezagsrelaties to filter
+     * @return <i>persoon</i> without irrelevant <i>gezagsrelaties</i>
+     */
+    private Persoon excludeIndirectGezagsrelaties(Persoon persoon) {
+        var gezagsrelaties = persoon.getGezag().stream()
+            .filter(gezagsrelatie ->
+                switch (gezagsrelatie) {
+                    case GezagNietTeBepalen it -> it.getMinderjarige().map(Minderjarige::getBurgerservicenummer).equals(persoon.getBurgerservicenummer());
+                    case TijdelijkGeenGezag it -> it.getMinderjarige().map(Minderjarige::getBurgerservicenummer).equals(persoon.getBurgerservicenummer());
+                    default -> true;
+
+            })
+            .toList();
+        persoon.setGezag(gezagsrelaties);
+        return persoon;
     }
 
     @ExceptionHandler

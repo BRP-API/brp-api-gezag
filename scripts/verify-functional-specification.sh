6d87dea7d958916d97e6eba94d218d17ba023513
Leroy Truong <leroytruong@cupofcode.io>
2025-07-16 10:48:03 +0200

    406 ci(cucumber): add scenarios under `features/test` to CI

--- scripts/verify-functional-specification.sh
+++ scripts/verify-functional-specification.sh
@@ -4,16 +4,30 @@ PARAMS="{ \
     \"apiUrl\": \"http://localhost:8080/api/v1/opvragenBevoegdheidTotGezag\" \
 }"
 
-npx cucumber-js -f json:./test-reports/cucumber-js/gezag-deprecated/test-result.json \
+npx cucumber-js -f json:./test-reports/cucumber-js/gezag-deprecated/specs-result.json \
                 -f summary:./test-reports/cucumber-js/gezag-deprecated/test-result-summary.txt \
                 -f summary \
                 features/specs \
                 --profile GezagApiDeprecated \
                 --world-parameters "$PARAMS"
 
-npx cucumber-js -f json:./test-reports/cucumber-js/gezag/test-result.json \
+npx cucumber-js -f json:./test-reports/cucumber-js/gezag-deprecated/test-result.json \
+                -f summary:./test-reports/cucumber-js/gezag-deprecated/test-result-summary.txt \
+                -f summary \
+                features/test \
+                --profile GezagApiDeprecated \
+                --world-parameters "$PARAMS"
+
+npx cucumber-js -f json:./test-reports/cucumber-js/gezag/specs-result.json \
                 -f summary:./test-reports/cucumber-js/gezag/test-result-summary.txt \
                 -f summary \
                 features/specs \
+                --profile GezagApi \
+                --world-parameters "$PARAMS"
+
+npx cucumber-js -f json:./test-reports/cucumber-js/gezag/test-result.json \
+                -f summary:./test-reports/cucumber-js/gezag/test-result-summary.txt \
+                -f summary \
+                features/test \
                 --profile GezagApi \
                 --world-parameters "$PARAMS"
